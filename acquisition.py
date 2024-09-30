92038e80e8726fd81b82f7a81ddd7043d908e575
Xu Zhihao <144765578+KyoChigo@users.noreply.github.com>
2024-09-30 17:27:34 +0900

    Add files via upload
    beta 0.1
    ver 20240930

---
+++ acquisition.py
@@ -0,0 +1,175 @@
+#coding: utf-8
+'''
+收购系统函数测试
+'''
+
+from dev.magicmq.pyspigot import PySpigot as ps # type: ignore
+import org.bukkit.inventory # type: ignore
+from org.bukkit.plugin.java import JavaPlugin # type: ignore
+from org.bukkit import Bukkit, ChatColor # type: ignore
+from com.earth2me.essentials import Essentials # type: ignore
+from decimal import Decimal, ROUND_DOWN  # Essentials经济系统处理用
+from util.anvilgui import anvilInputer # AnvilGUI
+from net.wesjd.anvilgui import AnvilGUI # AnvilGUI Java Lib
+import math
+
+script_config = ps.config.loadConfig('acquisition.yml')
+    
+class calculate:
+    def __init__(self):
+        self.dictGoods = ["DIAMOND", "GOLD_INGOT", "IRON_INGOT", "COAL", "OAK_LOG", "BONE", "DIRT"]
+        self.dictPrice = [20.00, 6.00, 1.50, 0.85, 1.00, 1.00, 0.08]
+        self.dictEffic = [0.0004, 0.0004, 0.0004, 0.0003, 0.0002, 0.0002, 0.0016]
+
+    def calPrice(self, count=1, countHistory=0, priceInit=1.00, effic=0.0002, residue=0.00):
+        totalPrice = Decimal('0.00')   # 总获益金额
+        residue = Decimal(residue)
+        overflow = False    # 判断是否超过本周期余额
+        countSold = 0
+        for i in range(count):
+            priceNow = priceInit * math.exp(-effic*(i+countHistory+1))  # 当前单价
+            priceNow = Decimal(priceNow).quantize(Decimal('0.00'), rounding=ROUND_DOWN)
+            if totalPrice + priceNow > residue:
+                overflow = True
+                break
+            totalPrice += priceNow
+            countSold += 1
+        if countSold != 0:
+            unitPrice = round(totalPrice / countSold, 2)
+        else:
+            unitPrice = Decimal('0.00')
+
+        return [countSold, totalPrice, unitPrice, overflow]
+
+    def calHistory(self, numberInit, t, g=1, tau=0.5):
+        "numberInit: 初始数量, g: 逆时间系数(越大则临界点附近变化越剧烈), tau: 临界点"
+        return int(math.floor(numberInit * (math.exp(g * (t - tau)) + 1) / (math.exp(g * (t + 1 - tau)) + 1)))
+
+
+def test(sender, label, args):
+    player = sender.getPlayer()  # 获取玩家对象
+    ItemInHand = player.getInventory().getItemInMainHand()  # 手中物品
+
+    if ItemInHand.getType().toString() in calculate().dictGoods:
+        player.sendMessage(ChatColor.translateAlternateColorCodes('&', u"&e[DC收购]&a 正在收购 " + ItemInHand.getType().toString() + u"！"))
+        ItemInHandNumber = 0  # 手中物品的总数
+        # 遍历玩家的背包
+        for item in player.getInventory().getContents():
+            if item is not None and item.getType() == ItemInHand.getType():
+                ItemInHandNumber += item.getAmount()
+
+        inputGUI = anvilInputer()
+        itemNumHolder = [0]  # 使用列表保存itemNum
+
+        def clickHandler(slot, stateSnapshot):
+            if slot == AnvilGUI.Slot.OUTPUT: # GUI输入
+                text = stateSnapshot.getText() # 获取玩家输入
+                if text is not None:
+                    try:
+                        itemNumHolder[0] = int(text)
+                    except ValueError:
+                        player.sendMessage(ChatColor.translateAlternateColorCodes('&', u"&e[DC收购]&c 输入格式不正确！"))
+
+                    if int(text) > ItemInHandNumber: # 限制出售物品数量的上下限
+                        player.sendMessage(ChatColor.translateAlternateColorCodes('&', u"&e[DC收购]&c 您只有 " + str(ItemInHandNumber) + u" 个 "
+                                                                                  + ItemInHand.getType().toString() + u" ！"))
+                        return [AnvilGUI.ResponseAction.replaceInputText(u"您没有那么多物品！")]
+                    elif int(text) <= 0:
+                        return [AnvilGUI.ResponseAction.replaceInputText(u"不能设置非正数！")]
+                    else:
+                        return [AnvilGUI.ResponseAction.close()]
+                    
+                else:
+                    return [AnvilGUI.ResponseAction.replaceInputText(u"请输入出售数量")]
+        inputGUI.onClick(clickHandler)
+        
+        # 设置关闭界面时的回调函数（必须是一个输入）
+        def closeHandler(stateSnapshot):
+            if itemNumHolder[0] >= 1 and itemNumHolder[0] <= ItemInHandNumber:
+                configSection = script_config.getConfigurationSection(str(player.getName()))
+
+                if configSection is not None:   # 检查玩家是否有收购记录
+                    tempDict = configSection.getValues(True)
+                    residue = Decimal(str(tempDict["RESIDUE"]))
+                    if ItemInHand.getType().toString() in tempDict.keys():
+                        testHistory = sum(tempDict[ItemInHand.getType().toString()])
+                    else:
+                        testHistory = 0
+                else:
+                    residue = Decimal('10000.00')   # 默认本周期剩余收购额度
+                    tempDict = {"RESIDUE": residue}
+                    testHistory = 0
+                itemID = calculate().dictGoods.index(ItemInHand.getType().toString())
+                itemNum = itemNumHolder[0]
+                testPriceInit = calculate().dictPrice[itemID]
+                testEffic = calculate().dictEffic[itemID]
+                calResult = calculate().calPrice(count=itemNum, priceInit=testPriceInit, effic=testEffic, countHistory=testHistory, residue=residue)
+                countSold = calResult[0]
+                testprice = calResult[1].quantize(Decimal('0.00'), rounding=ROUND_DOWN)
+                testUnitPrice = calResult[2]
+                overflow = calResult[3]
+
+                player.getInventory().removeItem(org.bukkit.inventory.ItemStack(ItemInHand.getType(), countSold)) # 删除出售物品
+                residue -= testprice
+                if countSold == 0:
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', u"&e[DC收购]&a 由于当前商品单价超过剩余收购额度，未能售出物品。"))
+                else:
+                    if overflow:
+                        player.sendMessage(ChatColor.translateAlternateColorCodes('&', u"&e[DC收购]&a 由于剩余收购额度不足，仅售出了一部分物品。"))
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', u"&e[DC收购]&a 售出 " + str(countSold) + u" 个 " + ItemInHand.getType().toString() 
+                                                                            + u"，获得 " + str(testprice) + u" DC币！平均单价为 " + str(testUnitPrice) + u" DC币。"))
+                    player.sendMessage(ChatColor.translateAlternateColorCodes('&', u"&e[DC收购]&a 本周期剩余收购额度 " + str(residue) + u" DC币。"))
+
+                tempDict["RESIDUE"] = residue
+                try:
+                    tempDict[ItemInHand.getType().toString()][0] += countSold
+                except:
+                    tempList = [int(countSold)] + [0 for _ in range(23)]
+                    tempDict[ItemInHand.getType().toString()] = tempList
+                script_config.set(str(player.getName()), tempDict)
+                script_config.save()
+                
+                user = Bukkit.getServer().getPluginManager().getPlugin("Essentials").getUser(player)
+                user.giveMoney(testprice)
+            else:
+                player.sendMessage(ChatColor.translateAlternateColorCodes('&', u"&e[DC收购]&a 取消收购。"))
+
+        inputGUI.onClose(lambda stateSnapshot : ps.scheduler.runTaskLater(closeHandler, 1, stateSnapshot)) # 重要：延迟1ticks再打开，否则不会触发Inventory事件！
+        inputGUI.title(u"DC收购窗口")
+        inputGUI.text(u"在此输入出售数量")
+        inputGUI.open(player)
+
+    else:
+        player.sendMessage(ChatColor.translateAlternateColorCodes('&', u"&e[DC收购]&c 手中物品不在收购范围内！"))
+    
+    return True
+
+
+def newCycle(sender, label, args):
+    player = sender.getPlayer()  # 获取玩家对象
+    configDict = script_config.getValues(True)
+
+    for sectionName in configDict:
+        if "." not in sectionName:
+            playerName = sectionName
+            playerConfig = script_config.getConfigurationSection(str(sectionName)).getValues(True)
+
+        elif str(sectionName)[len(playerName)+1:] in calculate().dictGoods:
+            goodsName = str(sectionName)[len(playerName)+1:]
+            section = playerConfig[goodsName]
+            tempList = [0]
+            for i in range(23):
+                tempList.append(calculate().calHistory(section[i], t=i, g=0.7, tau=4))
+            script_config.set(str(sectionName), tempList)
+
+        elif str(sectionName)[len(playerName)+1:] == "RESIDUE": # 确定新周期余额，待补充
+            script_config.set(str(sectionName), Decimal('10000.00'))
+            
+    script_config.save()
+    player.sendMessage(ChatColor.translateAlternateColorCodes('&', u"&e[DC收购]&a 系统已进入新周期！"))
+
+    return True
+
+
+ps.command.registerCommand(test, "functest")
+ps.command.registerCommand(newCycle, "newcycle")
