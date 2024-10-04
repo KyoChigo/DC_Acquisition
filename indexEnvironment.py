88c8b7ae7d856091342f9465bd096f38e380e3e3
Xu Zhihao <144765578+KyoChigo@users.noreply.github.com>
2024-10-04 09:15:45 +0900

    Create indexEnvironment.py

---
+++ indexEnvironment.py
@@ -0,0 +1,61 @@
+"""用于计算物价环境指数的程序"""
+
+import math
+import datetime
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+holiday = [datetime.date(2024, 4, 4) + datetime.timedelta(days=int(day - 1)) for day in range(2)] \
+        + [datetime.date(2024, 5, 1) + datetime.timedelta(days=int(day - 1)) for day in range(4)] \
+        + [datetime.date(2024, 6, 8) + datetime.timedelta(days=int(day - 1)) for day in range(2)] \
+        + [datetime.date(2024, 9, 15) + datetime.timedelta(days=int(day - 1)) for day in range(2)] \
+
+
+def is_weekend(date):
+    "判断是否为周末"
+    return date.weekday() >= 5
+
+def Gauss(x, mu, sigma):
+    "正规化Gauss分布"
+    return math.exp(-(x - mu) ** 2 / (2 * sigma ** 2)) / (sigma * math.sqrt(2 * math.pi))
+
+def superGauss(x, mu, sigma):
+    "超级Gauss分布"
+    n = 3
+    return math.exp(-((x - mu) ** (2 * n)) / (2 * sigma ** 2))
+
+def indexEnvi(time_list):
+    "物价环境指数"
+    alpha = 0.15
+    beta = 0.3
+    index_list = []
+    for time in time_list:
+        index = 1
+        index -= alpha * superGauss(time, 31, 16 ** 4)
+        index -= alpha * superGauss(time, 211, 16 ** 4)
+        index -= alpha * superGauss(time, 365 + 31, 16 ** 4)
+        index -= alpha * Gauss(time, 273, 1.6)
+        date = datetime.date(2024, 1, 1) + datetime.timedelta(days=int(time - 1))
+        if date in holiday:
+            index *= 0.96
+        elif is_weekend(date):
+            index *= 0.98   # 周末物价下调
+        index_list.append(index)
+    
+    return index_list
+
+time_values = list(range(1, 366))
+dates = [datetime.date(2024, 1, 1) + datetime.timedelta(days=int(day - 1)) for day in time_values]
+index_values = indexEnvi(time_values)
+
+plt.figure(figsize=(15, 6))
+plt.scatter(dates, index_values, label='indexEnvi(time)', color='b', s=3)
+plt.title('Plot of indexEnvi(time)')
+plt.xlabel('date')
+plt.ylabel('environmental index')
+plt.xlim(datetime.date(2024, 1, 1), datetime.date(2024, 12, 31))
+plt.ylim(0.7, 1.1)
+plt.grid(True)
+plt.legend()
+plt.xticks(rotation=45)
+plt.show()
